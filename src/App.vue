597bdf5829e43f2cbd6dd132653a0511fae19f2f
Dowmeynek <106123479+Dowmeynek@users.noreply.github.com>
2023-10-09 16:14:32 +0800

    dev1

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,25 @@
+
 <template>
-  <div id="app">
-    <v-app>
-      <v-banner
-        v-if="deferredPrompt"
-        color="info"
-        dark
-        class="text-left"
-      >
-        Install our Apps
+	<div id="app">
 
-        <template v-slot:actions>
-          <v-btn text @click="dismiss">Dismiss</v-btn>
-          <v-btn text @click="install">Install</v-btn>
-        </template>
-      </v-banner>
-      <router-view>
+<dec/>
+<top/>
+<mid/>
 
-      </router-view>
-    </v-app>
-  </div>
-</template>
-<script>
-import Cookies from "js-cookie";
+  
+	</div>
+  </template>
+  <script>
+
+import dec from"@/components/dec.vue";
+import top from"@/components/laman/top.vue";
+import mid from"@/components/laman/mid.vue";
 export default {
-  name: "App",
-  data() {
-    return {
-      deferredPrompt: null
-    };
-  },
-  created() {
-    window.addEventListener("beforeinstallprompt", e => {
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      this.deferredPrompt = e;
-      if (Cookies.get("add-to-home-screen") === undefined) {
-    this.deferredPrompt = e;
-    }
-    });
-window.addEventListener("appinstalled", () => {
-      this.deferredPrompt = null;
-    });
-  },
-  methods: {
-    async dismiss() {
-      Cookies.set("add-to-home-screen", null, { expires: 15 });
-      this.deferredPrompt = null;
-      this.deferredPrompt = null;
-    },
-    async install() {
-      this.deferredPrompt.prompt();
-    }
+  components: {
+    dec,top,mid
   }
 };
 </script>
+
+
+
