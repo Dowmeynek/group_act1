b6d12a1f014050c10db26a4b6ed08c524bf1a671
KateLogatoc09 <144669024+KateLogatoc09@users.noreply.github.com>
2023-10-10 18:19:08 +0800

    add vue
    add vue

---
+++ src/components/add.vue
@@ -0,0 +1,34 @@
+<style>
+/* Import your CSS files here */
+  @import '@/assets/assets/css/styles.min.css';
+  @import '@/assets/assets/images/favicon.png';
+</style>
+
+<script>
+  export default {
+    name: "Style",
+  mounted() {
+      this.loadScripts();
+  },
+  methods: {
+      loadScripts() {
+        const scriptUrls = [
+        '/public/js/sidebarmenu.js',
+        '/public/js/app.min.js',
+        '/public/js/dashboard.js',
+
+        ];
+
+        const head = document.getElementsByTagName('head')[0];
+
+        scriptUrls.forEach((scriptUrl) => {
+          const script = document.createElement('script');
+          script.src = scriptUrl;
+          script.async = true;
+          head.appendChild(script);
+        });
+      },
+  
+    },
+  };
+</script>
