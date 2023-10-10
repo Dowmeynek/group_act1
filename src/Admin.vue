c698d141402f76435cd171be0df77b0eb601559c
KateLogatoc09 <144669024+KateLogatoc09@users.noreply.github.com>
2023-10-10 20:23:46 +0800

    fix
    fix

--- src/Admin.vue
+++ src/Admin.vue
@@ -10,9 +10,9 @@
   </template>
   <script>
 
-import add from"@/components/add.vue";
-import navy from"@/components/laman/navy.vue";
-import foot from"@/components/laman/footer.vue";
+import add from"@/components/Admin/add.vue";
+import navy from"@/components/Admin/navy.vue";
+import foot from"@/components/Admin/footer.vue";
 //import prod from"@/components/laman/prod.vue";
 export default {
   components: {
