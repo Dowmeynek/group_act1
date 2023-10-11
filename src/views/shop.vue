88aa711eb6511ffdb9891434771abe45455bd2d1
Dowmeynek <106123479+Dowmeynek@users.noreply.github.com>
2023-10-11 17:54:24 +0800

    main

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+        <!-- Start Hero Section -->
+        <div class="hero">
+      <div class="container">
+        <div class="row justify-content-between">
+						<div class="col-lg-5">
+							<div class="intro-excerpt">
+								<h1>Shop</h1>
+								<p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
+								<p><a href="/" class="btn btn-secondary me-2">Back to Home</a></p>
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="hero-img-wrap">
+                <img :src="require('@/assets/img/icons/couch.png')" class="img-fluid">
+
+							</div>
+						</div>
+					</div>
+      </div>
+    </div>
+    <!-- End Hero Section -->
   <div class="product-section">
       <div class="container">
           <div class="row">
